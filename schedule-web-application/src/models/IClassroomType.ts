dffde31d4fe8f7d9842aca2eff6a29615a7b4b62
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-04-11 21:08:11 +0300

    Add and update react models

--- schedule-web-application/src/models/IClassroomType.ts
+++ schedule-web-application/src/models/IClassroomType.ts
@@ -1,16 +1,5 @@
-import {IDiscipline} from "./IDiscipline";
-import {ITime} from "./ITime";
-
 export interface IClassroomType {
     id: number
     name: string
 }
 
-export interface ILesson {
-    id: number
-    subgroup: number
-    timetableId: number
-    isChanged: boolean
-    time: ITime
-    discipline: IDiscipline
-}

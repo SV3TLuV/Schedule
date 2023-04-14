35985f22486e0170b27e9c0d5039ab3fdd5e2638
Twolfi <volk222klov@gmail.com>
2023-04-14 17:24:26 +0300

    Update GroupViewModel.cs

--- schedule-web-application/src/components/LoginForm/LoginForm.tsx
+++ schedule-web-application/src/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,8 @@
-import React from "react";
 import {Controller, SubmitHandler, useForm} from "react-hook-form";
 import {ILoginCommand} from "../../features/commands/ILoginCommand";
-import {Button, Card, Col, Form} from "react-bootstrap";
-import {BsPersonCircle} from "react-icons/bs";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import {ValidationMessage} from "../../common/enums/ValidationMessage";
 import {loginFormValidationSchema} from "./LoginFormValidation";
 import {yupResolver} from "@hookform/resolvers/yup";
+import {Card, CardContent, CardHeader} from "@mui/material";
 
 export const LoginForm = () => {
     const {
@@ -24,58 +19,13 @@ export const LoginForm = () => {
     }
 
     return (
-        <Card className="mb-5" style={{ borderRadius: '24px', boxShadow: '5px 10px 20px rgb(41,73,106)' }}>
-            <Card.Header>
-                <Col className="col-md-8 col-xl-6 text-center mx-auto">
-                    <h2>Авторизация</h2>
-                </Col>
-            </Card.Header>
-            <Card.Body className="d-flex flex-column align-items-center">
-                <div className="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4">
-                    <BsPersonCircle />
-                </div>
-                <Form className="text-center" method="post" onSubmit={handleSubmit(onSubmit)}>
-                    <Controller
-                        name="login"
-                        control={control}
-                        render={({ field }) => (
-                            <Form.Group controlId="formLogin">
-                                <Form.Control
-                                    placeholder="Логин"
-                                    className="mb-3"
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                />
-                                <Form.Control.Feedback type="invalid">
-                                    {errors.login?.message}
-                                </Form.Control.Feedback>
-                            </Form.Group>
-                        )}
-                    />
-                    <Form.Group controlId="formPassword">
-                        <Controller
-                            name="password"
-                            control={control}
-                            render={({ field }) => (
-                                <Form.Group controlId="formPassword">
-                                    <Form.Control
-                                        placeholder="Пароль"
-                                        className="mb-3"
-                                        value={field.value}
-                                        onChange={field.onChange}
-                                    />
-                                    <Form.Control.Feedback type="invalid">
-                                        {"Лохи ебаные"}
-                                    </Form.Control.Feedback>
-                                </Form.Group>
-                            )}
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="d-block w-100 mb-3">
-                        Войти
-                    </Button>
-                </Form>
-            </Card.Body>
+        <Card>
+            <CardHeader>
+                <h2>Авторизация</h2>
+            </CardHeader>
+            <CardContent>
+
+            </CardContent>
         </Card>
     )
 }

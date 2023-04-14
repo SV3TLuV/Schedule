68fb6c2c054bfaa7e01e6ff94fac0f253798241f
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-04-14 17:09:43 +0300

    add tailwind

--- schedule-web-application/src/pages/LoginPage.tsx
+++ schedule-web-application/src/pages/LoginPage.tsx
@@ -1,20 +1,12 @@
 import {LoginForm} from "../components/LoginForm/LoginForm";
-import {Col, Container, Row} from "react-bootstrap";
+import React from "react";
 
 export const LoginPage = () => {
     return (
-        <Container
-            fluid
-            style={{ height: "100vh" }}
-            className="d-flex justify-content-center align-items-center"
-        >
-            <section className="flex-fill py-4 py-xl-5">
-                <Row className="justify-content-center">
-                    <Col md={6} xl={4}>
-                        <LoginForm />
-                    </Col>
-                </Row>
-            </section>
-        </Container>
+        <div className="flex justify-center items-center h-screen">
+            <div className="shadow-2xl">
+                <LoginForm/>
+            </div>
+        </div>
     )
 }

ba8911c19a0da538952e8b7df5c960be3356aa36
Twolfi <volk222klov@gmail.com>
2023-04-14 18:19:15 +0300

    Create LoginForm.tsx, LoginPage.tsx

--- schedule-web-application/src/pages/LoginPage.tsx
+++ schedule-web-application/src/pages/LoginPage.tsx
@@ -1,12 +1,10 @@
 import {LoginForm} from "../components/LoginForm/LoginForm";
-import React from "react";
 
 export const LoginPage = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="shadow-2xl">
-                <LoginForm/>
-            </div>
+        <div className="container-fluid d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center"
+             style={{ height: "100vh" }}>
+            <LoginForm/>
         </div>
     )
 }

e9bbd4d6826f5232fc91f5f72c3c39064ce51901
Twolfi <volk222klov@gmail.com>
2023-04-15 09:32:56 +0300

    Add editor page

--- schedule-web-application/src/components/GroupEditor/GroupEditor.tsx
+++ schedule-web-application/src/components/GroupEditor/GroupEditor.tsx
@@ -1,3 +1,26 @@
+import {GridColDef} from "@mui/x-data-grid";
+import {ICourse} from "../../features/models/ICourse";
+import {ISpecialityCode} from "../../features/models/ISpecialityCode";
+
+
+const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", hideable: true },
+    { field: "name", headerName: "Название", width: 160 },
+    { field: "enrollmentYear", headerName: "Год поступления", width: 160 },
+    {
+        field: "course",
+        headerName: "Курс",
+        width: 60,
+        renderCell: params => (params.value as ICourse).id
+    },
+    {
+        field: "specialityCode",
+        headerName: "Код специальности",
+        width:180,
+        renderCell: params => (params.value as ISpecialityCode).code
+    },
+]
+
 export const GroupEditor = () => {
     return (
         <h1>Едитор групп</h1>

4cbf8e0164725b85182bc5361cd2eb3c8c7986a2
Twolfi <volk222klov@gmail.com>
2023-05-12 22:36:57 +0300

    Add DisciplineTypeController.cs

---
+++ Schedule/Schedule.Api/Controllers/DisciplineTypeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedule.Application.Features.DisciplineTypes.Queries.Get;
+using Schedule.Application.Features.DisciplineTypes.Queries.GetList;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Models;
+
+namespace Schedule.Api.Controllers;
+
+public class DisciplineTypeController : BaseController
+{
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<DisciplineTypeViewModel>> Get(int id)
+    {
+        var query = new GetDisciplineTypeQuery(id);
+        return Ok(await Mediator.Send(query));
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedList<DisciplineTypeViewModel>>> GetAll(
+        [FromQuery] GetDisciplineTypeListQuery query)
+    {
+        return Ok(await Mediator.Send(query));
+    }
+}
---
+++ Schedule/Schedule.Application/Features/DisciplineTypes/Queries/Get/GetDisciplineTypeQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Schedule.Application.Features.Base.Validators;
+
+namespace Schedule.Application.Features.DisciplineTypes.Queries.Get;
+
+public sealed class GetDisciplineTypeQueryValidator : AbstractValidator<GetDisciplineTypeQuery>
+{
+    public GetDisciplineTypeQueryValidator()
+    {
+        RuleFor(query => query.Id)
+            .SetValidator(new IdValidator());
+    }
+}

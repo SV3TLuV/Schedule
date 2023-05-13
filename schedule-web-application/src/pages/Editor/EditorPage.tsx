cedd3a706418586ad1585fd65233d64cef525469
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-05-13 14:06:09 +0300

    Remove editors

---
+++ schedule-web-application/src/pages/Editor/EditorPage.tsx
@@ -0,0 +1,10 @@
+﻿import {Card, Col, Container, Row, Tab, Tabs} from "react-bootstrap";
+import styles from './EditorPage.module.css';
+
+export const EditorPage = () => {
+    return (
+        <Container className={styles.container}>
+
+        </Container>
+    )
+}

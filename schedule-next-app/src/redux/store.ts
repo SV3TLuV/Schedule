469d1e8e12fdede4b02fbb2678032af31f313f4c
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-05-14 21:20:36 +0300

    update next app from ssr with redux to client with redux and next 13

--- schedule-next-app/src/redux/store.ts
+++ schedule-next-app/src/redux/store.ts
@@ -1,21 +1,34 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {baseApi} from "@/services/baseApi";
-import {createWrapper} from "next-redux-wrapper";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/es/storage";
+import persistStore from "redux-persist/es/persistStore";
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
 
 const rootReducer = combineReducers({
     [baseApi.reducerPath]: baseApi.reducer,
 });
 
+const persistedReducer = persistReducer({
+    key: "schedule-next-root",
+    storage,
+    whitelist: [],
+}, rootReducer);
+
 export const makeStore = () => {
     return configureStore({
-        reducer: rootReducer,
+        reducer: persistedReducer,
         middleware: getDefaultMiddleware =>
-            getDefaultMiddleware()
-                .concat(baseApi.middleware),
+            getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+                }
+            }).concat(baseApi.middleware),
     });
 }
 
+export const store = makeStore()
+export const persistor = persistStore(store)
 export type AppStore = ReturnType<typeof makeStore>
 export type AppState = ReturnType<typeof rootReducer>
 export type AppDispatch = ReturnType<AppStore['dispatch']>
-export const wrapper = createWrapper<AppStore>(makeStore)
--- schedule-web-application/src/pages/LoginPage.tsx
+++ schedule-web-application/src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import {LoginForm} from "../components/LoginForm/LoginForm";
 export const LoginPage = () => {
     return (
         <div className="container-fluid d-flex d-xxl-flex justify-content-center align-items-center justify-content-xxl-center align-items-xxl-center"
-             style={{ height: "100vh" }}>
+             style={{ height: "calc(100vh - 72px)" }}>
             <LoginForm/>
         </div>
     )

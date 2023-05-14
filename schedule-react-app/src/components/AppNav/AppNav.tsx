2834536266f431a57b7d243f01cf48da5c392244
Twolfi <volk222klov@gmail.com>
2023-05-15 01:26:43 +0300

    updates

--- schedule-react-app/src/components/AppNav/AppNav.tsx
+++ schedule-react-app/src/components/AppNav/AppNav.tsx
@@ -1,35 +1,49 @@
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {BiTable} from "react-icons/all";
+import {useNavigation} from "../../hooks/useNavigation.ts";
+import {Routes} from "../../common/enums/Routes.ts";
 
 export const AppNav = () => {
+    const {navigateTo} = useNavigation()
+
+    const navigateToLogin = () => navigateTo(Routes.LOGIN)
+    const navigateToEditor = () => navigateTo(Routes.EDITOR)
+    const navigateToSchedule = () => navigateTo(Routes.SCHEDULE)
+    const navigateToReports = () => navigateTo(Routes.REPORTS)
+
     return (
-        <Navbar bg="light" expand="md" className="py-3">
+        <Navbar bg='light' expand='md' className='py-3'>
             <Container>
-                <Navbar.Brand href="#" className="navbar-brand d-flex align-items-center">
-                    <span className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon">
-                        <BiTable size="1em" />
+                <Navbar.Brand className='navbar-brand d-flex align-items-center'>
+                    <span className='bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon'>
+                        <BiTable size='1em' />
                     </span>
-                    <span className="fw-bolder">
+                    <span className='fw-bolder'>
                         <strong>Расписание</strong>
                     </span>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="navcol-2">
-                    <span className="visually-hidden">Toggle navigation</span>
-                    <span className="navbar-toggler-icon"/>
+                <Navbar.Toggle aria-controls='navcol-2'>
+                    <span className='visually-hidden'>Toggle navigation</span>
+                    <span className='navbar-toggler-icon'/>
                 </Navbar.Toggle>
-                <Navbar.Collapse id="navcol-2">
-                    <Nav className="ms-auto">
-                        <Nav.Link active>
+                <Navbar.Collapse id='navcol-2'>
+                    <Nav className='ms-auto'>
+                        <Nav.Link onClick={navigateToSchedule}>
                             Расписание
                         </Nav.Link>
-                        <Nav.Link active>
+                        <Nav.Link onClick={navigateToEditor}>
                             Редактор
                         </Nav.Link>
-                        <Nav.Link active>
+                        <Nav.Link onClick={navigateToReports}>
                             Отчеты
                         </Nav.Link>
                     </Nav>
-                    <Button className="ms-md-2" variant="primary" role="button">
+                    <Button
+                        onClick={navigateToLogin}
+                        className='ms-md-2'
+                        variant='primary'
+                        role='button'
+                    >
                         Войти
                     </Button>
                 </Navbar.Collapse>

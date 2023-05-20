bf26b0798c604a7080e9b86afc3200c599fa9714
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-05-20 15:58:13 +0300

    Fix classroom editor

--- schedule-react-app/src/components/screens/editor/classroom/AvailableClassroomsEditor.tsx
+++ schedule-react-app/src/components/screens/editor/classroom/AvailableClassroomsEditor.tsx
@@ -1,19 +1,53 @@
 import {usePaginationQuery} from "../../../../hooks/usePaginationQuery.ts";
 import {QueryFilter} from "../../../../common/enums/QueryFilter.ts";
 import {
+    useDeleteClassroomMutation,
     useGetClassroomsQuery,
 } from "../../../../store/apis/classroomApi.ts";
-import {ClassroomEditor} from "./ClassroomEditor.tsx";
+import {Container} from "react-bootstrap";
+import {DataGridWithPagination} from "../../../ui/DataGridWithPagination.tsx";
+import {columns} from "./columns.ts";
+import {CreateClassroomDialog} from "./dialogs/CreateClassroomDialog.tsx";
+import {UpdateClassroomDialog} from "./dialogs/UpdateClassroomDialog.tsx";
+import {EditorToolbar} from "../EditorToolbar.tsx";
+import {useState} from "react";
+import {IClassroom} from "../../../../features/models/IClassroom.ts";
+import {useDialog} from "../../../../hooks/useDialog.ts";
 
 export const AvailableClassroomsEditor = () => {
     const [paginationQuery, setPaginationQuery] = usePaginationQuery(QueryFilter.Available)
+    const [selected, setSelected] = useState<IClassroom>({} as IClassroom)
     const {data} = useGetClassroomsQuery(paginationQuery)
+    const [remove] = useDeleteClassroomMutation()
+    const createDialog = useDialog()
+    const updateDialog = useDialog()
+
+    const handleUpdate = (classroom: IClassroom) => {
+        setSelected(classroom)
+        updateDialog.show()
+    }
+
+    const handleDelete = (classroom: IClassroom) => remove(classroom.id)
 
     return (
-        <ClassroomEditor
-            data={data}
-            paginationQuery={paginationQuery}
-            setPaginationQuery={setPaginationQuery}
-        />
+        <Container style={{ height: 'calc(100vh - 114px)', padding: 0 }}>
+            <DataGridWithPagination
+                columns={columns}
+                list={data}
+                toolbar={() => (
+                    <EditorToolbar
+                        paginationQuery={paginationQuery}
+                        setPaginationQuery={setPaginationQuery}
+                        onCreate={createDialog.show}
+                    />
+                )}
+                paginationModel={paginationQuery}
+                onPaginationModelChange={setPaginationQuery}
+                onUpdate={handleUpdate}
+                onDelete={handleDelete}
+            />
+            <CreateClassroomDialog {...createDialog}/>
+            <UpdateClassroomDialog classroom={selected} {...updateDialog}/>
+        </Container>
     )
 }
--- schedule-react-app/src/components/screens/editor/classroom/DeletedClassroomsEditor.tsx
+++ schedule-react-app/src/components/screens/editor/classroom/DeletedClassroomsEditor.tsx
@@ -1,17 +1,30 @@
 import {usePaginationQuery} from "../../../../hooks/usePaginationQuery.ts";
 import {QueryFilter} from "../../../../common/enums/QueryFilter.ts";
 import {useGetClassroomsQuery} from "../../../../store/apis/classroomApi.ts";
-import {ClassroomEditor} from "./ClassroomEditor.tsx";
+import {Container} from "react-bootstrap";
+import {DataGridWithPagination} from "../../../ui/DataGridWithPagination.tsx";
+import {columns} from "./columns.ts";
+import {EditorToolbar} from "../EditorToolbar.tsx";
 
 export const DeletedClassroomsEditor = () => {
     const [paginationQuery, setPaginationQuery] = usePaginationQuery(QueryFilter.Deleted)
     const {data} = useGetClassroomsQuery(paginationQuery)
 
     return (
-        <ClassroomEditor
-            data={data}
-            paginationQuery={paginationQuery}
-            setPaginationQuery={setPaginationQuery}
-        />
+        <Container style={{ height: 'calc(100vh - 114px)', padding: 0 }}>
+            <DataGridWithPagination
+                columns={columns}
+                list={data}
+                toolbar={() => (
+                    <EditorToolbar
+                        paginationQuery={paginationQuery}
+                        setPaginationQuery={setPaginationQuery}
+                    />
+                )}
+                paginationModel={paginationQuery}
+                onPaginationModelChange={setPaginationQuery}
+                onRestore={() => console.log('restore')}
+            />
+        </Container>
     )
 }

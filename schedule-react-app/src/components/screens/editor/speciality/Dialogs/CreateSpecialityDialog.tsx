5b20ce75e83ea9f0eda9bdb3f636780f8f1c1c58
noH4uK90 <noh4uk90@gmail.com>
2023-05-20 18:09:37 +0300

    Add Group dialog and Speciality dialog

--- schedule-react-app/src/components/screens/editor/speciality/Dialogs/CreateSpecialityDialog.tsx
+++ schedule-react-app/src/components/screens/editor/speciality/Dialogs/CreateSpecialityDialog.tsx
@@ -1,10 +1,21 @@
 import {ISpeciality} from "../../../../../features/models/ISpeciality";
+import {useCreateSpecialityMutation} from "../../../../../store/apis/specialityApi";
+import {SpecialityForm} from "./SpecialityForm";
+import {emptySpeciality} from "../../../../../utils/defaultModels";
+import {IDialog} from "../../../../../features/models/IDialog";
 
-export const emptySpeciality: ISpeciality = {
-    id: 0,
-    code: '',
-    name: '',
-    maxTermId: 0,
-    disciplines: [],
-    isDeleted: false
+export const CreateSpecialityDialog = ({ open, close }: IDialog) => {
+    const [create] = useCreateSpecialityMutation()
+
+    const handleSave = (speciality: ISpeciality) => create(speciality)
+
+    return (
+        <SpecialityForm
+            title='Добавление специальности'
+            show={open}
+            speciality={emptySpeciality}
+            onClose={close}
+            onSave={handleSave}
+        />
+    )
 }

e56e4d1b5e2a99dccf340afb159a51f4d4168a2c
Twolfi <volk222klov@gmail.com>
2023-05-26 16:27:09 +0300

    fix restore in DeletedDisciplinesEditor.tsx

--- schedule-react-app/src/components/screens/editor/discipline/DeletedDisciplinesEditor.tsx
+++ schedule-react-app/src/components/screens/editor/discipline/DeletedDisciplinesEditor.tsx
@@ -2,10 +2,9 @@ import {Container} from "react-bootstrap";
 import {columns} from "./columns.ts";
 import {usePaginationQuery} from "../../../../hooks/usePaginationQuery.ts";
 import {QueryFilter} from "../../../../common/enums/QueryFilter.ts";
-import {useGetDisciplinesQuery} from "../../../../store/apis/disciplineApi.ts";
+import {useGetDisciplinesQuery, useRestoreDisciplineMutation} from "../../../../store/apis/disciplineApi.ts";
 import {EditorToolbar} from "../EditorToolbar.tsx";
 import {DataGridWithPagination} from "../../../ui/DataGridWithPagination.tsx";
-import {useRestoreClassroomTypeMutation} from "../../../../store/apis/classroomTypeApi.ts";
 import {IDiscipline} from "../../../../features/models/IDiscipline.ts";
 
 export const DeletedDisciplinesEditor = () => {
@@ -13,7 +12,7 @@ export const DeletedDisciplinesEditor = () => {
         filter: QueryFilter.Deleted
     })
     const {data} = useGetDisciplinesQuery(paginationQuery)
-    const [restore] = useRestoreClassroomTypeMutation()
+    const [restore] = useRestoreDisciplineMutation()
 
     const handleRestore = (discipline: IDiscipline) => restore(discipline.id)
 

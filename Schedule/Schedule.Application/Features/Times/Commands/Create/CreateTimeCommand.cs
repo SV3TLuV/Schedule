53cbc242687362ea5a38244c33875077cc2b8429
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-05-26 22:02:50 +0300

    fix create, update commands for time

--- Schedule/Schedule.Application/Features/Times/Commands/Create/CreateTimeCommand.cs
+++ Schedule/Schedule.Application/Features/Times/Commands/Create/CreateTimeCommand.cs
@@ -6,8 +6,8 @@ namespace Schedule.Application.Features.Times.Commands.Create;
 
 public sealed class CreateTimeCommand : IRequest<int>, IMapWith<Time>
 {
-    public required TimeSpan Start { get; set; }
-    public required TimeSpan End { get; set; }
+    public required string Start { get; set; }
+    public required string End { get; set; }
     public required int LessonNumber { get; set; }
     public required int Duration { get; set; }
     public required int TypeId { get; set; }

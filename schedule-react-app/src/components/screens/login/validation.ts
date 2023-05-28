085a8fcd4bf33be4af7c10b7b93cb209469a325c
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-05-28 11:25:19 +0300

    Fix login form

--- schedule-react-app/src/components/screens/login/validation.ts
+++ schedule-react-app/src/components/screens/login/validation.ts
@@ -1,7 +1,9 @@
-import * as Yup from 'yup'
 import {ValidationMessage} from "../../../common/enums/ValidationMessage.ts";
 
-export const loginFormValidationSchema = Yup.object().shape({
-    login: Yup.string().required(ValidationMessage.REQUIRED),
-    password: Yup.string().required(ValidationMessage.REQUIRED),
-})
+export const loginValidation = {
+    required: ValidationMessage.REQUIRED,
+}
+
+export const passwordValidation = {
+    required: ValidationMessage.REQUIRED,
+}

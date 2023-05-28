cd88ba7c4b796c17ade59bf937884a207ac8b42e
Twolfi <volk222klov@gmail.com>
2023-05-28 15:40:02 +0300

    Update timetableApi.ts

--- schedule-react-app/src/store/apis/timetableApi.ts
+++ schedule-react-app/src/store/apis/timetableApi.ts
@@ -13,12 +13,14 @@ export const timetableApi = baseApi.injectEndpoints({
                 url: `${ApiTags.Timetable}?${buildUrlArguments(query ?? {})}`,
                 method: HttpMethod.GET,
             }),
-            providesTags: result => [
+            providesTags: (result, _, arg) => [
                 ...(result?.items ?? []).map(({id}) => ({type: ApiTags.Timetable, id} as const)),
                 {
                     type: ApiTags.Timetable,
                     id: 'LIST',
-                    page: result?.pageNumber
+                    page: result?.pageNumber,
+                    groupId: arg?.groupId,
+                    dateId: arg?.dateId
                 }
             ]
         }),

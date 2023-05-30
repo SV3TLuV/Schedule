b677f2c844950a6da9ae2f30ae196da31d86906d
Twolfi <volk222klov@gmail.com>
2023-05-31 00:56:41 +0300

    fixs

--- schedule-react-app/src/components/screens/editor/lesson/forms/TemplateForm.tsx
+++ schedule-react-app/src/components/screens/editor/lesson/forms/TemplateForm.tsx
@@ -1,29 +1,64 @@
 import {ITemplate} from "../../../../../features/models/ITemplate.ts";
-import {Card} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import {LessonTemplateForm} from "./LessonTemplateForm.tsx";
+import {useCreateLessonTemplateMutation} from "../../../../../store/apis/lessonTemplateApi.ts";
+import {ILessonTemplate} from "../../../../../features/models/ILessonTemplate.ts";
+import {numberValidation} from "../dialogs/validation.ts";
 
 interface ITemplateForm {
     template: ITemplate
 }
 
 export const TemplateForm = ({template}: ITemplateForm) => {
+    const [create] = useCreateLessonTemplateMutation()
+
+    const handleCreate = () => {
+        const numbers =  template.lessonTemplates.map(lessonTemplate => lessonTemplate.number)
+        const number = Math.max(...numbers) + 1
+
+        if (numberValidation.validate(number) === true) {
+            create({
+                id: 0,
+                number: number,
+                subgroup: null,
+                templateId: template.id,
+                time: null,
+                discipline: null,
+                teacherClassrooms: []
+            } as ILessonTemplate)
+        }
+    }
+
     return (
         <Card
-            style={{ minWidth: '280px', maxWidth: '400px'}}
             className='text-center'
+            style={{
+                minWidth: '280px',
+                maxWidth: '400px',
+            }}
         >
             <Card.Header>
                 <Card.Title>
                     {template.groupNames}
                 </Card.Title>
             </Card.Header>
-            <Card.Body className='py-1'>
+            <Card.Body
+                className='py-1'
+                style={{
+                    maxHeight: '720px',
+                    overflowX: 'hidden',
+                    overflowY: 'scroll'
+                }}
+            >
                 {template.lessonTemplates?.map(lessonTemplate => (
                     <LessonTemplateForm
                         key={lessonTemplate.id}
                         lessonTemplate={lessonTemplate}
                     />
                 ))}
+                <Button onClick={handleCreate}>
+                    Добавить пару
+                </Button>
             </Card.Body>
         </Card>
     )

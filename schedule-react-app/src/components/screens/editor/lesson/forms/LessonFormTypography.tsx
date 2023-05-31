1565f0929b4456e11241b7de8e5bf5a7c897e130
Twolfi <volk222klov@gmail.com>
2023-05-31 14:35:12 +0300

    fix LessonForm.tsx

--- schedule-react-app/src/components/screens/editor/lesson/forms/LessonFormTypography.tsx
+++ schedule-react-app/src/components/screens/editor/lesson/forms/LessonFormTypography.tsx
@@ -7,15 +7,14 @@ interface ILessonFormTypography {
 export const LessonFormTypography = ({text}: ILessonFormTypography) => {
     return (
         <Typography
-            align="center"
+            align='center'
             sx={{
-                color: "#b0b3b8",
-                height: "40px",
-                fontWeight: "400",
-                fontSize: "1rem",
-                lineHeight: "40px",
-                border: "1px solid #555555",
-                borderRadius: "5px"
+                height: '40px',
+                fontWeight: '400',
+                fontSize: '1rem',
+                lineHeight: '40px',
+                border: '1px solid #555555',
+                borderRadius: '5px'
             }}
         >
             {text}

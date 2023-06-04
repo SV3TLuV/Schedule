d846a76bc82ce18f0f32bbb991cc9fd0b6d62a8d
Twolfi <volk222klov@gmail.com>
2023-06-04 14:12:17 +0300

    Add error's output

--- Schedule/Schedule.Application/Features/Users/Commands/Login/LoginCommandHandler.cs
+++ Schedule/Schedule.Application/Features/Users/Commands/Login/LoginCommandHandler.cs
@@ -38,7 +38,7 @@ public sealed class LoginCommandHandler : IRequestHandler<LoginCommand, Authoriz
             .FirstOrDefaultAsync(e => e.Login == request.Login, cancellationToken);
 
         if (user is null || !BCrypt.Net.BCrypt.EnhancedVerify(request.Password, user.PasswordHash, HashType.SHA512))
-            throw new NotFoundException("Incorrect login or password");
+            throw new AuthorizationException();
 
         var command = new CreateSessionCommand
         {

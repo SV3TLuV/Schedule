840b4a280c08ecc2cc3684c8cc4d214af7fbe3ba
Twolfi <volk222klov@gmail.com>
2023-06-05 21:18:53 +0300

    Add logout all sessions on update user

---
+++ Schedule/Schedule.Application/Features/Users/Notifications/UserSessionRevocationNotificationHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Application.Features.Sessions.Commands.Delete;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Users.Notifications;
+
+public sealed class UserSessionRevocationNotificationHandler
+    : INotificationHandler<UserSessionRevocationNotification>
+{
+    private readonly IScheduleDbContext _context;
+    private readonly IMediator _mediator;
+
+    public UserSessionRevocationNotificationHandler(
+        IScheduleDbContext context,
+        IMediator mediator)
+    {
+        _context = context;
+        _mediator = mediator;
+    }
+    
+    public async Task Handle(UserSessionRevocationNotification notification,
+        CancellationToken cancellationToken)
+    {
+        var sessionIds = await _context.Set<Session>()
+            .AsNoTrackingWithIdentityResolution()
+            .Where(e => e.UserId == notification.UserId)
+            .Select(e => e.SessionId)
+            .ToListAsync(cancellationToken);
+
+        foreach (var sessionId in sessionIds)
+        {
+            var command = new DeleteSessionCommand(sessionId);
+            await _mediator.Send(command, cancellationToken);
+        }
+    }
+}

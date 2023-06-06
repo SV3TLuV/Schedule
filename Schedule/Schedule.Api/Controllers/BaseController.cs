5d6316f3261dfe5774d985b7404d3c9f6e8a73aa
Twolfi <volk222klov@gmail.com>
2023-06-06 11:11:48 +0300

    fixes

--- Schedule/Schedule.Api/Controllers/BaseController.cs
+++ Schedule/Schedule.Api/Controllers/BaseController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Schedule.Api.Common;
+using Schedule.Application.Features.Reports.Queries.GetReportForDate;
+using Schedule.Application.ViewModels;
 
 namespace Schedule.Api.Controllers;
 
@@ -11,4 +13,14 @@ namespace Schedule.Api.Controllers;
 public abstract class BaseController : ControllerBase
 {
     protected IMediator Mediator => HttpContext.RequestServices.GetRequiredService<IMediator>();
+}
+
+public sealed class ReportController : BaseController
+{
+    [HttpGet]
+    public async Task<IResult> Get([FromQuery] GetReportForDateQuery query)
+    {
+        var report = await Mediator.Send(query);
+        return Results.File(report.Content, report.ContentType, report.ReportName);
+    }
 }

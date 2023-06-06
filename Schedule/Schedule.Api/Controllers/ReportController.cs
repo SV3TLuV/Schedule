a37fdb2e86a5c55ba8157a3adf424b5c00abaff5
Twolfi <volk222klov@gmail.com>
2023-06-06 21:39:40 +0300

    Add authorization

--- Schedule/Schedule.Api/Controllers/ReportController.cs
+++ Schedule/Schedule.Api/Controllers/ReportController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Schedule.Application.Features.Reports.Queries.GetTimetableReport;
 
 namespace Schedule.Api.Controllers;
 
 public sealed class ReportController : BaseController
 {
+    [Authorize]
     [HttpGet("timetable")]
     public async Task<IResult> Get([FromQuery] GetTimetableReportQuery query)
     {

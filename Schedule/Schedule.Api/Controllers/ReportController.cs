d86ff4e82ce1dfbd045a6ac3bfeb2de3d8cb5241
Twolfi <volk222klov@gmail.com>
2023-06-06 11:12:30 +0300

    Add reports

---
+++ Schedule/Schedule.Api/Controllers/ReportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Schedule.Application.Features.Reports.Queries.GetReportForDate;
+
+namespace Schedule.Api.Controllers;
+
+public sealed class ReportController : BaseController
+{
+    [HttpGet]
+    public async Task<IResult> Get([FromQuery] GetReportForDateQuery query)
+    {
+        var report = await Mediator.Send(query);
+        return Results.File(report.Content, report.ContentType, report.ReportName);
+    }
+}

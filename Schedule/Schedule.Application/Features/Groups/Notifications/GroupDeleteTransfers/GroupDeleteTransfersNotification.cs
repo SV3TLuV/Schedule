8973f88bacd72666537eccb0c17369403c697354
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-06-07 20:23:04 +0300

    fixes

---
+++ Schedule/Schedule.Application/Features/Groups/Notifications/GroupDeleteTransfers/GroupDeleteTransfersNotification.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Groups.Notifications.GroupDeleteTransfers;
+
+public sealed record GroupDeleteTransfersNotification(int Id) : INotification;

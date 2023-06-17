f23c300e14a00091b0658c098a9111632f73803a
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-06-17 16:20:58 +0300

    fast fix

---
+++ Schedule/Schedule.Application/Features/Groups/Notifications/GroupUpdateForMergedGroups/GroupUpdateForMergedGroupsNotification.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Groups.Notifications.GroupUpdateForMergedGroups;
+
+public sealed record GroupUpdateForMergedGroupsNotification(int Id) : INotification;

224d73b15c03595c2cadf90ddbdd7f947f3f4d37
Twolfi <volk222klov@gmail.com>
2023-06-26 21:32:02 +0300

    Updates for docker on production

--- schedule.web.application/src/configuration.ts
+++ schedule.web.application/src/configuration.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://api:5001';
-export const SIGNAL_R_URL = 'wss://api:5001';
+export const API_URL = 'http://192.168.0.29:5000';
+export const SIGNAL_R_URL = 'ws://192.168.0.29:5000';

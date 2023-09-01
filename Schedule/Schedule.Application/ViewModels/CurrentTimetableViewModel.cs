91279a74c0cc3a2388a9ddd1b60de93b5d7b2e9f
Twolfi <Volk222kloV@gmail.com>
2023-09-01 13:29:19 +0300

    fix names

--- Schedule/Schedule.Application/ViewModels/CurrentTimetableViewModel.cs
+++ Schedule/Schedule.Application/ViewModels/CurrentTimetableViewModel.cs
@@ -3,6 +3,6 @@
 public class CurrentTimetableViewModel
 {
     public required ICollection<GroupViewModel> Groups { get; set; }
-    public string GroupNames => string.Join(" ", Groups.Select(g => g.Name));
+    public string GroupNames => string.Join(" ", Groups.Select(g => g.Name).Distinct());
     public required ICollection<GroupedViewModel<DateViewModel, TimetableViewModel>> Dates { get; set; }
 }

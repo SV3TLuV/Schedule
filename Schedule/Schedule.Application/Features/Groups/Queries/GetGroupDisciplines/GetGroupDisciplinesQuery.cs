c478159de4088226ae4ed50947fc553587f81353
Twolfi <Volk222kloV@gmail.com>
2023-09-04 18:11:12 +0300

    Fix bug when disciplines don't show for change lesson

---
+++ Schedule/Schedule.Application/Features/Groups/Queries/GetGroupDisciplines/GetGroupDisciplinesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Schedule.Application.ViewModels;
+
+namespace Schedule.Application.Features.Groups.Queries.GetGroupDisciplines;
+
+public sealed record GetGroupDisciplinesQuery : IRequest<ICollection<DisciplineViewModel>>
+{
+    public int GroupId { get; set; }
+}

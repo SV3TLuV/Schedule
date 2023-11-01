9066d591eaad42aaf644bd6720eec6cf1b919e29
noH4uK90 <noh4uk90@gmail.com>
2023-11-01 20:26:32 +0300

    Add Commands for Discipline Name

--- Schedule/Schedule.Application/Features/DisciplineNames/Commands/Update/UpdateDisciplineNameCommand.cs
+++ Schedule/Schedule.Application/Features/DisciplineNames/Commands/Update/UpdateDisciplineNameCommand.cs
@@ -1,6 +1,26 @@
-﻿namespace Schedule.Application.Features.DisciplineNames.Commands.Update;
+﻿using AutoMapper;
+using MediatR;
+using Schedule.Application.Common.Attributes;
+using Schedule.Application.Features.Base;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
 
-public class UpdateDisciplineNameCommand
+namespace Schedule.Application.Features.DisciplineNames.Commands.Update;
+
+[SignalRNotification(typeof(DisciplineName), CommandTypes.Update)]
+public sealed class UpdateDisciplineNameCommand : IRequest<Unit>, IMapWith<DisciplineName>
 {
-    
+    public required int Id { get; set; }
+    public required string Name { get; set; } = null!;
+
+    public void Map(Profile profile)
+    {
+        profile.CreateMap<DisciplineName, UpdateDisciplineNameCommand>()
+            .ForMember(command => command.Id, expression =>
+                expression.MapFrom(disciplineName => disciplineName.DisciplineNameId));
+
+        profile.CreateMap<UpdateDisciplineNameCommand, DisciplineName>()
+            .ForMember(disciplineName => disciplineName.DisciplineNameId, expression =>
+                expression.MapFrom(command => command.Id));
+    }
 }

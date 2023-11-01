077dbab3d28d0bd8f7ddde5d1d7febe059af85f6
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-11-01 20:53:14 +0300

    Add Migration

--- Schedule/Schedule.Persistence/Migrations/20231101175244_OptimizeDisciplinesTable.Designer.cs
+++ Schedule/Schedule.Persistence/Migrations/20231101175244_OptimizeDisciplinesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Schedule.Persistence.Context;
 
@@ -11,9 +12,11 @@ using Schedule.Persistence.Context;
 namespace Schedule.Persistence.Migrations
 {
     [DbContext(typeof(ScheduleDbContext))]
-    partial class ScheduleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231101175244_OptimizeDisciplinesTable")]
+    partial class OptimizeDisciplinesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

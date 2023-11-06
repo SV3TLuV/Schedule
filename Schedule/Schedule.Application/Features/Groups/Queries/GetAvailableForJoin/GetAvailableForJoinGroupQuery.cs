77cebbfb970cf6bb6f8261186b21128c851cf46c
Twolfi <67441287+SV3TLuV@users.noreply.github.com>
2023-11-06 12:46:01 +0300

    fix bug with GetAvailableForJoinGroupQuery.cs, when try add not showed groups, when try update for group after 11, does't show groups after 9

--- Schedule/Schedule.Application/Features/Groups/Queries/GetAvailableForJoin/GetAvailableForJoinGroupQuery.cs
+++ Schedule/Schedule.Application/Features/Groups/Queries/GetAvailableForJoin/GetAvailableForJoinGroupQuery.cs
@@ -6,6 +6,7 @@ namespace Schedule.Application.Features.Groups.Queries.GetAvailableForJoin;
 public sealed record GetAvailableForJoinGroupQuery : IRequest<GroupViewModel[]>
 {
     public int? GroupId { get; set; }
-    public int TermId { get; set; }
+    public int EnrollmentYear { get; set; }
     public int SpecialityId { get; set; }
+    public bool IsAfterEleven { get; set; }
 }

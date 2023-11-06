a27bd4e5d1cfddcf33e235e6beadc6ea060236f6
Twolfi <Volk222kloV@gmail.com>
2023-11-06 15:39:03 +0300

    fix bug, when authorization expired, u try download report and u get 401, because auto refresh doesn't work

---
+++ schedule.web.application/src/features/commands/IDownloadTimetableReport.ts
@@ -0,0 +1,4 @@
+export interface IDownloadTimetableReport {
+    startDateId: number
+    endDateId: number
+}
--- schedule.web.application/src/store/apis/reportApi.ts
+++ schedule.web.application/src/store/apis/reportApi.ts
@@ -1,13 +1,49 @@
-import {downloadFile} from "../../utils/downloadFile.ts";
 import {ApiTags} from "./apiTags.ts";
+import {baseApi} from "./baseApi.ts";
+import {HttpMethod} from "../../common/enums";
+import {baseQuery} from "../fetchBaseQueryWithReauth.ts";
+import {FetchBaseQueryError} from "@reduxjs/toolkit/dist/query/react";
+import {IDownloadTimetableReport} from "../../features/commands";
+import {buildUrlArguments} from "../../utils/buildUrlArguments.ts";
 
-export async function downloadTimetableReport(startDateId: number, endDateId: number, accessToken: string) {
-    return await downloadFile(
-        `${import.meta.env.VITE_API_URL}/api/${ApiTags.Report}/timetable`,
-        {
-            startDateId: startDateId,
-            endDateId: endDateId,
-        },
-        accessToken
-    )
-}
+export const reportApi = baseApi.injectEndpoints({
+    endpoints: builder => ({
+        downloadTimetableReport: builder.mutation<void, IDownloadTimetableReport | void>({
+            queryFn: async (command, api, extraOptions) => {
+                const response = await baseQuery({
+                    url: `${ApiTags.Report}/timetable?${buildUrlArguments(command ?? {})}`,
+                    method: HttpMethod.GET,
+                    responseHandler: response => response.blob()
+                }, api, extraOptions)
+
+                const headers = response!.meta?.response?.headers;
+
+                if (headers) {
+                    const header = headers.get('content-disposition') ?? '';
+                    const regex = /filename[^*]?=\s*["']?(.*?)["'];?/;
+                    const matches = header.match(regex);
+
+                    if (matches && matches.length > 1) {
+                        const fileName = matches[1];
+
+                        const blob = response.data as Blob
+                        const downloadUrl = window.URL.createObjectURL(blob)
+                        const link = document.createElement('a')
+                        link.href = downloadUrl
+                        link.setAttribute('download', fileName)
+                        document.body.appendChild(link)
+                        link.click()
+                        link.remove()
+                        window.URL.revokeObjectURL(downloadUrl)
+                    }
+                }
+
+                return { error: response.error as FetchBaseQueryError }
+            },
+        }),
+    })
+})
+
+export const {
+    useDownloadTimetableReportMutation
+} = reportApi

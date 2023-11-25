c922e25735e04ec065c0049f1881e6e75829c6e5
SV3TLUV <Volk222kloV@gmail.com>
2023-11-25 18:07:46 +0300

    take out code for saving file to function saveFile

--- schedule.web.application/src/store/apis/reportApi.ts
+++ schedule.web.application/src/store/apis/reportApi.ts
@@ -5,6 +5,7 @@ import {baseQuery} from "../fetchBaseQueryWithReauth.ts";
 import {FetchBaseQueryError} from "@reduxjs/toolkit/dist/query/react";
 import {IDownloadTimetableReport} from "../../features/commands";
 import {buildUrlArguments} from "../../utils/buildUrlArguments.ts";
+import {saveFile} from "../../utils/saveFile.ts";
 
 export const reportApi = baseApi.injectEndpoints({
     endpoints: builder => ({
@@ -16,25 +17,17 @@ export const reportApi = baseApi.injectEndpoints({
                     responseHandler: response => response.blob()
                 }, api, extraOptions)
 
-                const headers = response!.meta?.response?.headers;
+                const headers = response!.meta?.response?.headers
 
                 if (headers) {
-                    const header = headers.get('content-disposition') ?? '';
-                    const regex = /filename[^*]?=\s*["']?(.*?)["'];?/;
-                    const matches = header.match(regex);
+                    const header = headers.get('content-disposition') ?? ''
+                    const regex = /filename[^*]?=\s*["']?(.*?)["'];?/
+                    const matches = header.match(regex)
 
                     if (matches && matches.length > 1) {
-                        const fileName = matches[1];
-
+                        const fileName = matches[1]
                         const blob = response.data as Blob
-                        const downloadUrl = window.URL.createObjectURL(blob)
-                        const link = document.createElement('a')
-                        link.href = downloadUrl
-                        link.setAttribute('download', fileName)
-                        document.body.appendChild(link)
-                        link.click()
-                        link.remove()
-                        window.URL.revokeObjectURL(downloadUrl)
+                        saveFile(fileName, blob)
                     }
                 }
 

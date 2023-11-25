0046e45d3bd641e9577df335938688150b29f9da
SV3TLUV <Volk222kloV@gmail.com>
2023-11-25 21:08:01 +0300

    fix blob

--- Schedule/Schedule.Application/Features/Specialities/Commands/Import/ImportSpecialityCommand.cs
+++ Schedule/Schedule.Application/Features/Specialities/Commands/Import/ImportSpecialityCommand.cs
@@ -2,9 +2,7 @@ using MediatR;
 
 namespace Schedule.Application.Features.Specialities.Commands.Import;
 
-public class ImportSpecialityCommand : IRequest<Unit>
+public sealed class ImportSpecialityCommand : IRequest<Unit>
 {
     public required byte[] Content { get; set; }
-    public required string ContentType { get; set; }
-    public required string ReportName { get; set; }
 }
--- Schedule/Schedule.Application/Features/Teachers/Commands/Import/ImportTeacherCommand.cs
+++ Schedule/Schedule.Application/Features/Teachers/Commands/Import/ImportTeacherCommand.cs
@@ -3,9 +3,7 @@ using Schedule.Application.ViewModels;
 
 namespace Schedule.Application.Features.Teachers.Commands.Import;
 
-public class ImportTeacherCommand : IRequest<Unit>
+public sealed class ImportTeacherCommand : IRequest<Unit>
 {
     public required byte[] Content { get; set; }
-    public required string ContentType { get; set; }
-    public required string ReportName { get; set; }
 }

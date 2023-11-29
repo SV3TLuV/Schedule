abf6834ae4c6947ff17bd4ad83f1c899f33f11f3
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2023-11-29 20:01:23 +0300

    Add useless tests

---
+++ Schedule/Schedule.Tests/Tests/Features/Users/Commands/UserCreateCommandTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Application.Features.Users.Commands.Create;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+using Schedule.Tests.Common;
+
+namespace Schedule.Tests.Tests.Features.Users.Commands;
+
+public sealed class UserCreateCommandTests
+{
+    [Fact]
+    public async Task Test_CreateUser()
+    {
+        //Arrange
+        var context = TestContainer.Resolve<IScheduleDbContext>();
+        var mapper = TestContainer.Resolve<IMapper>();
+        var command = new CreateUserCommand
+        {
+            Login = "TestUser",
+            Password = "TestUser",
+            RoleId = 1
+        };
+
+        //Act
+        var handler = new CreateUserCommandHandler(context, mapper);
+        var id = await handler.Handle(command, default);
+
+        //Assert
+        var user = await context.Set<User>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e =>
+                e.UserId == id &&
+                e.Login == command.Login &&
+                e.RoleId == command.RoleId);
+
+        Assert.NotNull(user);
+    }
+}

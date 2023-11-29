bbe358b2e8deee823246b0c64a43261ee84377e0
SV3TLUV <Volk222kloV@gmail.com>
2023-11-29 20:01:23 +0300

    Add useless tests

---
+++ Schedule/Schedule.Tests/Tests/Features/Specialities/Commands/SpecialityCreateCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Application.Features.Specialities.Commands.Create;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+using Schedule.Tests.Common;
+
+namespace Schedule.Tests.Tests.Features.Specialities.Commands;
+
+public sealed class SpecialityCreateCommandTests
+{
+    [Fact]
+    public async Task Test_CreateSpeciality()
+    {
+        //Arrange
+        var context = TestContainer.Resolve<IScheduleDbContext>();
+        var mapper = TestContainer.Resolve<IMapper>();
+        var command = new CreateSpecialityCommand
+        {
+            Code = "09.02.07",
+            Name = "ИСПП",
+            MaxTermId = 8
+        };
+
+        //Act
+        var handler = new CreateSpecialityCommandHandler(context, mapper);
+        var id = await handler.Handle(command, default);
+
+        //Assert
+        var user = await context.Set<Speciality>()
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e =>
+                e.SpecialityId == id &&
+                e.Code == command.Code &&
+                e.Name == command.Name &&
+                e.MaxTermId == command.MaxTermId);
+
+        Assert.NotNull(user);
+    }
+}

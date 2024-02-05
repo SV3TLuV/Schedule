111bd0faed1ffd02328e44003c32c4a86e58736a
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-02-05 20:54:48 +0300

    Remove unused code

--- api.schedule/schedule-main-service/internal/controller/day/handler/get_list/get_day_list.go
+++ api.schedule/schedule-main-service/internal/controller/day/handler/get_list/get_day_list.go
@@ -4,33 +4,9 @@ import (
 	"Api/internal/middleware"
 	"Api/internal/repository"
 	"encoding/json"
-	"github.com/gorilla/schema"
 	"github.com/jmoiron/sqlx"
 	"net/http"
 )
-
-type getDayListRequest struct {
-	ID uint8
-}
-
-func (r *getDayListRequest) Validate() error {
-	if r.ID <= 0 || r.ID > 255 {
-		return middleware.BadRequest
-	}
-
-	return nil
-}
-
-func fromRequest(r *http.Request) (*getDayListRequest, error) {
-	var request getDayListRequest
-	decoder := schema.NewDecoder()
-	err := decoder.Decode(&request, r.URL.Query())
-	if err != nil {
-		return nil, middleware.BadRequest
-	}
-
-	return &request, nil
-}
 
 func GetDays(db *sqlx.DB, rep repository.Repository) http.HandlerFunc {
 	return middleware.ErrorMiddleware(func(writer http.ResponseWriter, request *http.Request) error {

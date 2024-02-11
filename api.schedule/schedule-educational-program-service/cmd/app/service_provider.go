52e2be899edaf2ff01a8e0b75202b41b2147a826
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-02-12 00:46:15 +0300

    Register repositories

--- api.schedule/schedule-educational-program-service/cmd/app/service_provider.go
+++ api.schedule/schedule-educational-program-service/cmd/app/service_provider.go
@@ -3,6 +3,10 @@ package main
 import (
 	trmsqlx "github.com/avito-tech/go-transaction-manager/drivers/sqlx/v2"
 	"github.com/avito-tech/go-transaction-manager/trm/v2/manager"
+	"schedule-educational-program-service/cmd/app/internal/services/discipline"
+	"schedule-educational-program-service/cmd/app/internal/services/discipline_code"
+	"schedule-educational-program-service/cmd/app/internal/services/discipline_type"
+	"schedule-educational-program-service/cmd/app/internal/services/speciality"
 	"schedule-educational-program-service/cmd/app/internal/services/term"
 	"schedule-educational-program-service/pkg/db/postgresql"
 	"schedule-educational-program-service/pkg/migrator"
@@ -11,11 +15,15 @@ import (
 const migrationPath = "file://migrations/postgresql"
 
 type serviceProvider struct {
-	config           *Config
-	postgresqlClient *postgresql.Client
-	migrator         migrator.Migrator
-	trManager        *manager.Manager
-	termRepository   term.Repository
+	config                   *Config
+	postgresqlClient         *postgresql.Client
+	migrator                 migrator.Migrator
+	trManager                *manager.Manager
+	termRepository           term.Repository
+	disciplineRepository     discipline.Repository
+	disciplineCodeRepository discipline_code.Repository
+	disciplineTypeRepository discipline_type.Repository
+	specialityRepository     speciality.Repository
 }
 
 func newServiceProvider() *serviceProvider {
@@ -25,14 +33,40 @@ func newServiceProvider() *serviceProvider {
 }
 
 func (p *serviceProvider) init() {
+	p.initConfig()
+	p.initPostgresClient()
+	p.initMigrator()
+	p.initTrManager()
+	p.initRepositories()
+}
+
+func (p *serviceProvider) initConfig() {
 	p.config = newConfig()
+}
+
+func (p *serviceProvider) initPostgresClient() {
 	p.postgresqlClient = postgresql.New(
 		p.config.Postgres.User,
 		p.config.Postgres.Password,
 		p.config.Postgres.Database,
 		p.config.Postgres.Host,
 		p.config.Postgres.Port)
+}
+
+func (p *serviceProvider) initMigrator() {
 	p.migrator = migrator.New(migrationPath, p.config.Postgres.URL())
+}
+
+func (p *serviceProvider) initTrManager() {
 	p.trManager = manager.Must(trmsqlx.NewDefaultFactory(p.postgresqlClient.Connection))
-	p.termRepository = term.NewRepo(p.postgresqlClient.Connection, trmsqlx.DefaultCtxGetter)
+}
+
+func (p *serviceProvider) initRepositories() {
+	db, getter := p.postgresqlClient.Connection, trmsqlx.DefaultCtxGetter
+
+	p.termRepository = term.NewRepo(db, getter)
+	p.disciplineRepository = discipline.NewRepo(db, getter)
+	p.disciplineCodeRepository = discipline_code.NewRepo(db, getter)
+	p.disciplineTypeRepository = discipline_type.NewRepo(db, getter)
+	p.specialityRepository = speciality.NewRepo(db, getter)
 }

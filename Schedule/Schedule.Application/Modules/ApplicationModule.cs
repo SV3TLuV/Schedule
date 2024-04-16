36a7aa0e9de30ecc33a964128306892a11eb46a6
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 19:50:39 +0300

    Update dependencies

--- Schedule/Schedule.Application/Modules/ApplicationModule.cs
+++ Schedule/Schedule.Application/Modules/ApplicationModule.cs
@@ -5,7 +5,10 @@ using FluentValidation;
 using MediatR.Extensions.Autofac.DependencyInjection;
 using MediatR.Extensions.Autofac.DependencyInjection.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Schedule.Application.Common.Interfaces;
 using Schedule.Application.Common.Mappings;
+using Schedule.Application.Services;
+using Schedule.Core.Common.Interfaces;
 
 namespace Schedule.Application.Modules;
 
@@ -20,6 +23,18 @@ public sealed class ApplicationModule : Module
             .Create(ThisAssembly)
             .WithAllOpenGenericHandlerTypesRegistered()
             .Build());
+
+        builder.RegisterType<DateInfoService>()
+            .As<IDateInfoService>()
+            .SingleInstance();
+
+        builder.RegisterType<TokenService>()
+            .As<ITokenService>()
+            .SingleInstance();
+
+        builder.RegisterType<HashPasswordService>()
+            .As<IHashPasswordService>()
+            .SingleInstance();
 
         var services = new ServiceCollection();
 

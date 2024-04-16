cac5ed6f03bb7caf5067c61b9ce89d53d0418c33
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 13:47:32 +0300

    Remove sql server configurations

--- Schedule/Schedule.Persistence/Context/ScheduleDbContext.cs
+++ Schedule/Schedule.Persistence/Context/ScheduleDbContext.cs
@@ -69,10 +69,6 @@ public partial class ScheduleDbContext : DbContext, IScheduleDbContext
 
     public virtual DbSet<WeekType> WeekTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("User ID=postgres;Password=P@ssw0rd;Host=host.docker.internal;Port=5432;Database=schedule;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("en_US.UTF-8");

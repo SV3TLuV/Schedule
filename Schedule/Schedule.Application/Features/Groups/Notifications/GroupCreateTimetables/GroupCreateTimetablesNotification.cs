6b7606083c9d72c390def089f628e23e15e79621
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 23:18:48 +0300

    Delete unused group notification

--- Schedule/Schedule.Application/Features/Groups/Notifications/GroupCreateTimetables/GroupCreateTimetablesNotification.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Schedule.Application.Features.Groups.Notifications.GroupCreateTimetables;
-
-public sealed record GroupCreateTimetablesNotification(int Id) : INotification;
--- Schedule/Schedule.Application/Features/Groups/Notifications/GroupUpdateForMergedGroups/GroupUpdateForMergedGroupsNotification.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Schedule.Application.Features.Groups.Notifications.GroupUpdateForMergedGroups;
-
-public sealed record GroupUpdateForMergedGroupsNotification(int Id) : INotification;

8a8931db0436c4de573d3bb49dba8f48e117c908
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 23:33:47 +0300

    Delete unused lesson notifications

--- Schedule/Schedule.Application/Features/Lessons/Notifications/LessonUpdateForUnitedGroups/LessonUpdateForUnitedGroupsNotification.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Schedule.Application.Features.Lessons.Notifications.LessonUpdateForUnitedGroups;
-
-public sealed record LessonUpdateForUnitedGroupsNotification(int LessonId) : INotification;

1e2d5c7929f8d9cf499b388cbc482b2f0fed54f2
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 23:18:19 +0300

    Delete unused teacher queries

--- Schedule/Schedule.Application/Features/Teachers/Queries/Get/GetTeacherQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Teachers.Queries.Get;
-
-public sealed record GetTeacherQuery(int Id) : IRequest<TeacherViewModel>;
--- Schedule/Schedule.Application/Features/Teachers/Queries/GetTeacherReport/GetTeacherReportQuery.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Teachers.Queries.GetTeacherReport;
-
-public sealed record GetTeacherReportQuery : IRequest<ReportViewModel>;

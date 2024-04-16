1dcd589789cfba4575d1d51b09927577ffad9c04
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 22:52:43 +0300

    Fix GetAccountListQuery.cs

--- Schedule/Schedule.Application/Features/Accounts/Queries/GetList/GetAccountListQuery.cs
+++ Schedule/Schedule.Application/Features/Accounts/Queries/GetList/GetAccountListQuery.cs
@@ -1,8 +1,13 @@
 ﻿using MediatR;
 using Schedule.Application.Features.Base.Queries.Paginated;
 using Schedule.Application.ViewModels;
+using Schedule.Core.Common.Enums;
 using Schedule.Core.Models;
 
 namespace Schedule.Application.Features.Accounts.Queries.GetList;
 
-public sealed record GetAccountListQuery : PaginatedQuery, IRequest<PagedList<AccountViewModel>>;
+public sealed record GetAccountListQuery : PaginatedQuery, IRequest<PagedList<AccountViewModel>>
+{
+    public required QueryFilter Filter { get; init; } = QueryFilter.Available;
+    public string? Search { get; set; }
+}

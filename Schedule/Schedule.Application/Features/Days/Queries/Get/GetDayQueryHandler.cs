ac513f7d03d54d844bbdb92408374afd2f755e31
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 21:44:34 +0300

    Fix day commands and queries

--- Schedule/Schedule.Application/Features/Days/Queries/Get/GetDayQueryHandler.cs
+++ Schedule/Schedule.Application/Features/Days/Queries/Get/GetDayQueryHandler.cs
@@ -8,26 +8,18 @@ using Schedule.Core.Models;
 
 namespace Schedule.Application.Features.Days.Queries.Get;
 
-public sealed class GetDayQueryHandler : IRequestHandler<GetDayQuery, DayViewModel>
+public sealed class GetDayQueryHandler(IScheduleDbContext context, IMapper mapper)
+    : IRequestHandler<GetDayQuery, DayViewModel>
 {
-    private readonly IScheduleDbContext _context;
-    private readonly IMapper _mapper;
-
-    public GetDayQueryHandler(IScheduleDbContext context, IMapper mapper)
-    {
-        _context = context;
-        _mapper = mapper;
-    }
-
     public async Task<DayViewModel> Handle(GetDayQuery request, CancellationToken cancellationToken)
     {
-        var day = await _context.Set<Day>()
-            .AsNoTrackingWithIdentityResolution()
+        var day = await context.Days
+            .AsNoTracking()
             .FirstOrDefaultAsync(e => e.DayId == request.Id, cancellationToken);
 
         if (day is null)
             throw new NotFoundException(nameof(Day), request.Id);
 
-        return _mapper.Map<DayViewModel>(day);
+        return mapper.Map<DayViewModel>(day);
     }
 }
--- Schedule/Schedule.Application/Features/Days/Queries/GetCurrent/GetCurrentDayQueryHandler.cs
+++ Schedule/Schedule.Application/Features/Days/Queries/GetCurrent/GetCurrentDayQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Schedule.Application.Common.Interfaces;
@@ -8,28 +9,19 @@ using Schedule.Core.Models;
 
 namespace Schedule.Application.Features.Days.Queries.GetCurrent;
 
-public sealed class GetCurrentDayQueryHandler : IRequestHandler<GetCurrentDayQuery, DayViewModel>
+public sealed class GetCurrentDayQueryHandler(
+    IScheduleDbContext context,
+    IDateInfoService dateInfoService,
+    IMapper mapper)
+    : IRequestHandler<GetCurrentDayQuery, DayViewModel>
 {
-    private readonly IScheduleDbContext _context;
-    private readonly IDateInfoService _dateInfoService;
-    private readonly IMapper _mapper;
-
-    public GetCurrentDayQueryHandler(IScheduleDbContext context,
-        IDateInfoService dateInfoService,
-        IMapper mapper)
-    {
-        _context = context;
-        _dateInfoService = dateInfoService;
-        _mapper = mapper;
-    }
-
     public async Task<DayViewModel> Handle(GetCurrentDayQuery request,
         CancellationToken cancellationToken)
     {
-        var currentDayId = _dateInfoService.CurrentDayId;
-        var day = await _context.Set<Day>()
-            .AsNoTrackingWithIdentityResolution()
+        var currentDayId = dateInfoService.CurrentDayId;
+        var day = await context.Days
+            .AsNoTracking()
             .FirstOrDefaultAsync(e => e.DayId == currentDayId, cancellationToken);
-        return _mapper.Map<DayViewModel>(day);
+        return mapper.Map<DayViewModel>(day);
     }
 }

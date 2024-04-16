00ce30465fa40864a6e783a6380b4508235bac7b
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 22:05:20 +0300

    Add ChangePasswordCommand.cs

---
+++ Schedule/Schedule.Application/Features/Accounts/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Accounts.Commands.ChangePassword;
+
+public sealed class ChangePasswordCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+    public required string Password { get; set; }
+}

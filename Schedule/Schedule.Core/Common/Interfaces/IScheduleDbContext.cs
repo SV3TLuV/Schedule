a7de6ade980228324e35a5218337c177ea264f72
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 18:02:02 +0300

    Update IScheduleDbContext.cs

--- Schedule/Schedule.Core/Common/Interfaces/IScheduleDbContext.cs
+++ Schedule/Schedule.Core/Common/Interfaces/IScheduleDbContext.cs
@@ -1,11 +1,62 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Schedule.Core.Models;
 
 namespace Schedule.Core.Common.Interfaces;
 
 public interface IScheduleDbContext
 {
     DatabaseFacade Database { get; }
-    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+    DbSet<Account> Accounts { get; }
+
+    DbSet<Classroom> Classrooms { get; }
+
+    DbSet<Day> Days { get; }
+
+    DbSet<Discipline> Disciplines { get; }
+
+    DbSet<DisciplineCode> DisciplineCodes { get; }
+
+    DbSet<DisciplineName> DisciplineNames { get; }
+
+    DbSet<DisciplineType> DisciplineTypes { get; }
+
+    DbSet<Employee> Employees { get; }
+
+    DbSet<EmployeePermission> EmployeePermissions { get; }
+
+    DbSet<Group> Groups { get; }
+
+    DbSet<GroupTransfer> GroupTransfers { get; }
+
+    DbSet<Lesson> Lessons { get; }
+
+    DbSet<LessonChange> LessonChanges { get; }
+
+    DbSet<MiddleName> MiddleNames { get; }
+
+    DbSet<Name> Names { get; }
+
+    DbSet<Permission> Permissions { get; }
+
+    DbSet<Role> Roles { get; }
+
+    DbSet<Session> Sessions { get; }
+
+    DbSet<Speciality> Specialities { get; }
+
+    DbSet<Student> Students { get; }
+
+    DbSet<Surname> Surnames { get; }
+
+    DbSet<Teacher> Teachers { get; }
+
+    DbSet<Term> Terms { get; }
+
+    DbSet<Timetable> Timetables { get; }
+
+    DbSet<WeekType> WeekTypes { get; }
+
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- Schedule/Schedule.Persistence/Context/ScheduleDbContext.cs
+++ Schedule/Schedule.Persistence/Context/ScheduleDbContext.cs
@@ -15,55 +15,55 @@ public partial class ScheduleDbContext : DbContext, IScheduleDbContext
     {
     }
 
-    public virtual DbSet<Account> Accounts { get; set; }
+    public virtual DbSet<Account> Accounts { get; init; }
 
-    public virtual DbSet<Classroom> Classrooms { get; set; }
+    public virtual DbSet<Classroom> Classrooms { get; init; }
 
-    public virtual DbSet<Day> Days { get; set; }
+    public virtual DbSet<Day> Days { get; init; }
 
-    public virtual DbSet<Discipline> Disciplines { get; set; }
+    public virtual DbSet<Discipline> Disciplines { get; init; }
 
-    public virtual DbSet<DisciplineCode> DisciplineCodes { get; set; }
+    public virtual DbSet<DisciplineCode> DisciplineCodes { get; init; }
 
-    public virtual DbSet<DisciplineName> DisciplineNames { get; set; }
+    public virtual DbSet<DisciplineName> DisciplineNames { get; init; }
 
-    public virtual DbSet<DisciplineType> DisciplineTypes { get; set; }
+    public virtual DbSet<DisciplineType> DisciplineTypes { get; init; }
 
-    public virtual DbSet<Employee> Employees { get; set; }
+    public virtual DbSet<Employee> Employees { get; init; }
 
-    public virtual DbSet<EmployeePermission> EmployeePermissions { get; set; }
+    public virtual DbSet<EmployeePermission> EmployeePermissions { get; init; }
 
-    public virtual DbSet<Group> Groups { get; set; }
+    public virtual DbSet<Group> Groups { get; init; }
 
-    public virtual DbSet<GroupTransfer> GroupTransfers { get; set; }
+    public virtual DbSet<GroupTransfer> GroupTransfers { get; init; }
 
-    public virtual DbSet<Lesson> Lessons { get; set; }
+    public virtual DbSet<Lesson> Lessons { get; init; }
 
-    public virtual DbSet<LessonChange> LessonChanges { get; set; }
+    public virtual DbSet<LessonChange> LessonChanges { get; init; }
 
-    public virtual DbSet<MiddleName> MiddleNames { get; set; }
+    public virtual DbSet<MiddleName> MiddleNames { get; init; }
 
-    public virtual DbSet<Name> Names { get; set; }
+    public virtual DbSet<Name> Names { get; init; }
 
-    public virtual DbSet<Permission> Permissions { get; set; }
+    public virtual DbSet<Permission> Permissions { get; init; }
 
-    public virtual DbSet<Role> Roles { get; set; }
+    public virtual DbSet<Role> Roles { get; init; }
 
-    public virtual DbSet<Session> Sessions { get; set; }
+    public virtual DbSet<Session> Sessions { get; init; }
 
-    public virtual DbSet<Speciality> Specialities { get; set; }
+    public virtual DbSet<Speciality> Specialities { get; init; }
 
-    public virtual DbSet<Student> Students { get; set; }
+    public virtual DbSet<Student> Students { get; init; }
 
-    public virtual DbSet<Surname> Surnames { get; set; }
+    public virtual DbSet<Surname> Surnames { get; init; }
 
-    public virtual DbSet<Teacher> Teachers { get; set; }
+    public virtual DbSet<Teacher> Teachers { get; init; }
 
-    public virtual DbSet<Term> Terms { get; set; }
+    public virtual DbSet<Term> Terms { get; init; }
 
-    public virtual DbSet<Timetable> Timetables { get; set; }
+    public virtual DbSet<Timetable> Timetables { get; init; }
 
-    public virtual DbSet<WeekType> WeekTypes { get; set; }
+    public virtual DbSet<WeekType> WeekTypes { get; init; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {

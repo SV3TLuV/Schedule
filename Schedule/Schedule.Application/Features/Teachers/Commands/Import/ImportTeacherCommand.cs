4132173bd1db79380084093d2732df3c72cd1335
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 21:55:23 +0300

    Delete Teacher commands

--- Schedule/Schedule.Application/Features/Teachers/Commands/Import/ImportTeacherCommand.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Teachers.Commands.Import;
-
-public sealed class ImportTeacherCommand : IRequest<Unit>
-{
-    public required byte[] Content { get; set; }
-}

b9376f402989565a6b9fd03ab2be329343eee2aa
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 22:27:48 +0300

    Add RestoreAccountCommand.cs

---
+++ Schedule/Schedule.Application/Features/Accounts/Commands/Restore/RestoreAccountCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Core.Common.Exceptions;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Accounts.Commands.Restore;
+
+public sealed class RestoreAccountCommandHandler(
+    IScheduleDbContext context) : IRequestHandler<RestoreAccountCommand, Unit>
+{
+    public async Task<Unit> Handle(RestoreAccountCommand request, CancellationToken cancellationToken)
+    {
+        var account = await context.Accounts
+            .FirstOrDefaultAsync(e => e.AccountId == request.Id, cancellationToken);
+
+        if (account is null)
+            throw new NotFoundException(nameof(Account), request.Id);
+
+        account.IsDeleted = false;
+
+        context.Accounts.Update(account);
+        await context.SaveChangesAsync(cancellationToken);
+        return Unit.Value;
+    }
+}

7da5526296134893aee7d97e700d8061a4ceceed
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 23:09:31 +0300

    Delete unused term query

--- Schedule/Schedule.Application/Features/Terms/Queries/Get/GetTermQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Terms.Queries.Get;
-
-public sealed record GetTermQuery(int Id) : IRequest<TermViewModel>;

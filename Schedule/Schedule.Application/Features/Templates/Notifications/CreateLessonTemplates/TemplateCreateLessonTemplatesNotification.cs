48c60d1645d8fe79033f3012a0f2d658272f8638
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 23:28:12 +0300

    Delete unused template commands and queries

--- Schedule/Schedule.Application/Features/Templates/Notifications/CreateLessonTemplates/TemplateCreateLessonTemplatesNotification.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Schedule.Application.Features.Templates.Notifications.CreateLessonTemplates;
-
-public sealed record TemplateCreateLessonTemplatesNotification(int TemplateId) : INotification;

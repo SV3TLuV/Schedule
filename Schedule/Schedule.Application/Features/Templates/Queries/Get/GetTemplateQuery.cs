828e0c6247329da3e3e0ca252c6cb24e7af3edf8
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 23:28:12 +0300

    Delete unused template commands and queries

--- Schedule/Schedule.Application/Features/Templates/Queries/Get/GetTemplateQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Templates.Queries.Get;
-
-public sealed record GetTemplateQuery(int Id) : IRequest<TemplateViewModel>;

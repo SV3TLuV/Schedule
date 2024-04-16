c5caadd7b4e71b2e408750886d7705de1f52c357
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 21:55:23 +0300

    Delete Teacher commands

--- Schedule/Schedule.Application/Features/Teachers/Commands/Delete/DeleteTeacherCommand.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace Schedule.Application.Features.Teachers.Commands.Delete;
-
-public sealed record DeleteTeacherCommand(int Id) : IRequest<Unit>;
--- Schedule/Schedule.Application/Features/Teachers/Commands/Restore/RestoreTeacherCommand.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace Schedule.Application.Features.Teachers.Commands.Restore;
-
-public sealed record RestoreTeacherCommand(int Id) : IRequest<Unit>;

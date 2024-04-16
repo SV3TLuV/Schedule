d805ffdb4873840f5b3d8ad26c05e0c7d0799a2e
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-16 22:47:11 +0300

    Add UpdateEmployeePermissionCommand.cs

---
+++ Schedule/Schedule.Application/Features/Accounts/Commands/UpdateEmployeePermission/UpdateEmployeePermissionCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Core.Common.Enums;
+using Schedule.Core.Common.Exceptions;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Accounts.Commands.UpdateEmployeePermission;
+
+public sealed class UpdateEmployeePermissionCommandHandler(
+    IScheduleDbContext context) : IRequestHandler<UpdateEmployeePermissionCommand, Unit>
+{
+    public async Task<Unit> Handle(UpdateEmployeePermissionCommand request, CancellationToken cancellationToken)
+    {
+        var account = await context.Accounts
+            .AsNoTracking()
+            .Include(e => e.Employees)
+            .FirstOrDefaultAsync(e => e.AccountId == request.AccountId, cancellationToken);
+
+        if (account is null)
+            throw new NotFoundException(nameof(Account), request.AccountId);
+
+        if (account.RoleId != (int)AccountRole.Admin)
+            throw new NotAccessException();
+
+        var employeePermissions = request.PermissionIds
+            .Select(e => new EmployeePermission
+            {
+                EmployeeId = account.Employees.First().EmployeeId,
+                PermissionId = 0,
+            })
+            .ToArray();
+
+        await context.EmployeePermissions.AddRangeAsync(employeePermissions, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}

4c014fdbd79d9ad429da73e320ec5e319d3155ee
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 23:17:14 +0300

    Delete unused time query

--- Schedule/Schedule.Application/Features/Times/Queries/Get/GetTimeQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.Times.Queries.Get;
-
-public sealed record GetTimeQuery(int Id) : IRequest<TimeViewModel>;

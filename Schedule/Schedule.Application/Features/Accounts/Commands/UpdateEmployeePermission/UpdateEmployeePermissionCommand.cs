0653050c96b8e66e3ae84eebcb375bd390ca3c88
SV3TLUV <Volk222kloV@gmail.com>
2024-04-16 22:47:11 +0300

    Add UpdateEmployeePermissionCommand.cs

---
+++ Schedule/Schedule.Application/Features/Accounts/Commands/UpdateEmployeePermission/UpdateEmployeePermissionCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Accounts.Commands.UpdateEmployeePermission;
+
+public sealed class UpdateEmployeePermissionCommand : IRequest<Unit>
+{
+    public int AccountId { get; set; }
+    public required ICollection<int> PermissionIds { get; set; }
+}

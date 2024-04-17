4f90e1baf4645ae9f4acec3d106e4ee044b928c0
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-17 09:48:06 +0300

    Add GetNameListQuery.cs

---
+++ Schedule/Schedule.Application/Features/Names/Queries/GetNameListQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Schedule.Application.Features.Base.Queries.Paginated;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Names.Queries;
+
+public sealed record GetNameListQuery : PaginatedQuery, IRequest<PagedList<NameViewModel>>
+{
+    public string? Search { get; set; }
+}

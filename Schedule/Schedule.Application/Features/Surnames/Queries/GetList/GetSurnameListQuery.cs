2e6820c8a3cdd643b96eaf1d7b5d30df5d0385e7
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-17 09:50:58 +0300

    Add GetSurnameListQuery.cs

---
+++ Schedule/Schedule.Application/Features/Surnames/Queries/GetList/GetSurnameListQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Schedule.Application.Features.Base.Queries.Paginated;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Surnames.Queries.GetList;
+
+public sealed record GetSurnameListQuery : PaginatedQuery, IRequest<PagedList<SurnameViewModel>>
+{
+    public string? Search { get; set; }
+}

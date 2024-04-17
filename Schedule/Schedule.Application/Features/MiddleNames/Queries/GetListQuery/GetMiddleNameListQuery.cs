23734acdeae6680f30f43950758e8e8a7a4814c7
SV3TLUV <Volk222kloV@gmail.com>
2024-04-17 09:45:47 +0300

    Add GetMiddleNameListQuery.cs

---
+++ Schedule/Schedule.Application/Features/MiddleNames/Queries/GetListQuery/GetMiddleNameListQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Schedule.Application.Features.Base.Queries.Paginated;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.MiddleNames.Queries.GetListQuery;
+
+public sealed record GetMiddleNameListQuery : PaginatedQuery, IRequest<PagedList<MiddleNameViewModel>>
+{
+    public string? Search { get; set; }
+}

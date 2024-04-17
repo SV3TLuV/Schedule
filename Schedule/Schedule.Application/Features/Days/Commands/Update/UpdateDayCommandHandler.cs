c8bb555f49793c9fe615a6301b4db5fa0364f071
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-17 13:56:44 +0300

    Update Day model and UpdateDayCommandHandler.cs

--- Schedule/Schedule.Application/Features/Days/Commands/Update/UpdateDayCommandHandler.cs
+++ Schedule/Schedule.Application/Features/Days/Commands/Update/UpdateDayCommandHandler.cs
@@ -1,5 +1,4 @@
-﻿using AutoMapper;
-using MediatR;
+﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Schedule.Core.Common.Exceptions;
 using Schedule.Core.Common.Interfaces;
@@ -7,21 +6,23 @@ using Schedule.Core.Models;
 
 namespace Schedule.Application.Features.Days.Commands.Update;
 
-public sealed class UpdateDayCommandHandler(IScheduleDbContext context, IMapper mapper)
+public sealed class UpdateDayCommandHandler(IScheduleDbContext context)
     : IRequestHandler<UpdateDayCommand, Unit>
 {
     public async Task<Unit> Handle(UpdateDayCommand request, CancellationToken cancellationToken)
     {
-        var dayDbo = await context.Days
+        var day = await context.Days
             .AsNoTrackingWithIdentityResolution()
             .FirstOrDefaultAsync(e => e.DayId == request.Id, cancellationToken);
 
-        if (dayDbo is null)
+        if (day is null)
             throw new NotFoundException(nameof(Day), request.Id);
 
-        var day = mapper.Map<Day>(request);
+        day.IsStudy = request.IsStudy;
+
         context.Days.Update(day);
         await context.SaveChangesAsync(cancellationToken);
+
         return Unit.Value;
     }
 }

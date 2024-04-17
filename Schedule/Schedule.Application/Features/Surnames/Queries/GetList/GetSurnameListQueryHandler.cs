4e8ab9cbab78e67528622eedace396d8742dd6b7
SV3TLUV <Volk222kloV@gmail.com>
2024-04-17 09:50:58 +0300

    Add GetSurnameListQuery.cs

---
+++ Schedule/Schedule.Application/Features/Surnames/Queries/GetList/GetSurnameListQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Surnames.Queries.GetList;
+
+public sealed class GetSurnameListQueryHandler(
+    IScheduleDbContext context,
+    IMapper mapper) : IRequestHandler<GetSurnameListQuery, PagedList<SurnameViewModel>>
+{
+    public async Task<PagedList<SurnameViewModel>> Handle(GetSurnameListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.Surnames
+            .OrderBy(e => e.Value)
+            .Skip((request.Page - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .AsNoTracking();
+
+        if (request.Search is not null)
+        {
+            query = query.Where(e => e.Value.StartsWith(request.Search));
+        }
+
+        var surnames = await query
+            .ProjectTo<SurnameViewModel>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        var totalCount = await query.CountAsync(cancellationToken);
+
+        return new PagedList<SurnameViewModel>
+        {
+            PageSize = request.PageSize,
+            PageNumber = request.Page,
+            TotalCount = totalCount,
+            Items = surnames
+        };
+    }
+}

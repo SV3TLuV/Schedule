ab9f34ed6bae774f6e7dcedb3928ba2aaefcba07
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-17 22:32:11 +0300

    Some fixes

--- Schedule/Schedule.Application/Features/Lessons/Commands/Delete/DeleteLessonCommandValidator.cs
+++ Schedule/Schedule.Application/Features/Lessons/Commands/Delete/DeleteLessonCommandValidator.cs
@@ -1,13 +1,1 @@
-﻿using FluentValidation;
-using Schedule.Application.Features.Base.Validators;
-
-namespace Schedule.Application.Features.Lessons.Commands.Delete;
-
-public class DeleteLessonCommandValidator : AbstractValidator<DeleteLessonCommand>
-{
-    public DeleteLessonCommandValidator()
-    {
-        RuleFor(query => query.Id)
-            .SetValidator(new IdValidator());
-    }
-}
+﻿

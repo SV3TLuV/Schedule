74353f3ea176e3e7222118b2184332b5a4c8e2de
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-17 09:45:47 +0300

    Add GetMiddleNameListQuery.cs

---
+++ Schedule/Schedule.Application/Features/MiddleNames/Queries/GetListQuery/GetMiddleNameListQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.MiddleNames.Queries.GetListQuery;
+
+public sealed class GetMiddleNameListQueryHandler(
+    IScheduleDbContext context,
+    IMapper mapper) : IRequestHandler<GetMiddleNameListQuery, PagedList<MiddleNameViewModel>>
+{
+    public async Task<PagedList<MiddleNameViewModel>> Handle(GetMiddleNameListQuery request, CancellationToken cancellationToken)
+    {
+        var query = context.MiddleNames
+            .OrderBy(e => e.Value)
+            .Skip((request.Page - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .AsNoTracking();
+
+        if (request.Search is not null)
+        {
+            query = query.Where(e => e.Value.StartsWith(request.Search));
+        }
+
+        var middleNames = await query
+            .ProjectTo<MiddleNameViewModel>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        var totalCount = await query.CountAsync(cancellationToken);
+
+        return new PagedList<MiddleNameViewModel>
+        {
+            PageSize = request.PageSize,
+            PageNumber = request.Page,
+            TotalCount = totalCount,
+            Items = middleNames
+        };
+    }
+}

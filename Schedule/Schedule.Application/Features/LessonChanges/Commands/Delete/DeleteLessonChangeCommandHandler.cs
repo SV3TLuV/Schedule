3326fc0f675ad52bc60fda43e02a01668ba60285
SV3TLUV <Volk222kloV@gmail.com>
2024-04-19 20:05:04 +0300

    Add not implemented commands for lesson changes

---
+++ Schedule/Schedule.Application/Features/LessonChanges/Commands/Delete/DeleteLessonChangeCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Schedule.Core.Common.Interfaces;
+
+namespace Schedule.Application.Features.LessonChanges.Commands.Delete;
+
+public sealed class DeleteLessonChangeCommandHandler(
+    IScheduleDbContext context) : IRequestHandler<DeleteLessonChangeCommand, Unit>
+{
+    public async Task<Unit> Handle(DeleteLessonChangeCommand request, CancellationToken cancellationToken)
+    {
+        
+
+        return Unit.Value;
+    }
+}

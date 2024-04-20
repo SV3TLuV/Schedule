f150e99812ae67fee8e0d0de5ec166f38e1bce48
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-20 22:34:03 +0300

    Remove unused queries

--- Schedule/Schedule.Application/Features/WeekTypes/Queries/Get/GetWeekTypeQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.WeekTypes.Queries.Get;
-
-public sealed record GetWeekTypeQuery(int Id) : IRequest<WeekTypeViewModel>;

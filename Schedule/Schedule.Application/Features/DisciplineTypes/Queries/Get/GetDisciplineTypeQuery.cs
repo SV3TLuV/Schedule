83ffea9c273870bbc0d36126b59b68138681e9c8
SV3TLUV <Volk222kloV@gmail.com>
2024-04-20 23:17:26 +0300

    Delete unused query

--- Schedule/Schedule.Application/Features/DisciplineTypes/Queries/Get/GetDisciplineTypeQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Schedule.Application.ViewModels;
-
-namespace Schedule.Application.Features.DisciplineTypes.Queries.Get;
-
-public sealed record GetDisciplineTypeQuery(int Id) : IRequest<DisciplineTypeViewModel>;

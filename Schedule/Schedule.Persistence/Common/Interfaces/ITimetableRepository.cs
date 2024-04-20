f498a056da995d849367bcae452c876cb7f18ca3
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-20 16:15:38 +0300

    Add TimetableRepository.cs

---
+++ Schedule/Schedule.Persistence/Common/Interfaces/ITimetableRepository.cs
@@ -0,0 +1,9 @@
+﻿using Schedule.Core.Models;
+
+namespace Schedule.Persistence.Common.Interfaces;
+
+public interface ITimetableRepository
+{
+    public Task<int> CreateAsync(Timetable timetable, CancellationToken cancellationToken = default);
+    public Task UpdateEndedAsync(Timetable timetable, CancellationToken cancellationToken = default);
+}
---
+++ Schedule/Schedule.Persistence/Repositories/TimetableRepository.cs
@@ -0,0 +1,30 @@
+﻿using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
+
+namespace Schedule.Persistence.Repositories;
+
+public class TimetableRepository(
+    IScheduleDbContext context,
+    IDateInfoService dateInfoService) : Repository(context), ITimetableRepository
+{
+    public async Task<int> CreateAsync(Timetable timetable, CancellationToken cancellationToken = default)
+    {
+        timetable.Created = dateInfoService.CurrentDate;
+
+        var created = await Context.Timetables.AddAsync(timetable, cancellationToken);
+
+        await Context.SaveChangesAsync(cancellationToken);
+
+        return created.Entity.TimetableId;
+    }
+
+    public async Task UpdateEndedAsync(Timetable timetable, CancellationToken cancellationToken = default)
+    {
+        timetable.Ended = dateInfoService.CurrentDate;
+
+        Context.Timetables.Update(timetable);
+
+        await Context.SaveChangesAsync(cancellationToken);
+    }
+}

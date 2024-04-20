ab20e40435cde98bf717338fcf43d1d6c0bfd95e
SV3TLUV <Volk222kloV@gmail.com>
2024-04-20 21:13:28 +0300

    Update classroom and day commands

--- Schedule/Schedule.Application/Features/Classrooms/Commands/Restore/RestoreClassroomCommandHandler.cs
+++ Schedule/Schedule.Application/Features/Classrooms/Commands/Restore/RestoreClassroomCommandHandler.cs
@@ -3,24 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using Schedule.Core.Common.Exceptions;
 using Schedule.Core.Common.Interfaces;
 using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
 
 namespace Schedule.Application.Features.Classrooms.Commands.Restore;
 
-public sealed class RestoreClassroomCommandHandler(IScheduleDbContext context)
+public sealed class RestoreClassroomCommandHandler(IClassroomRepository classroomRepository)
     : IRequestHandler<RestoreClassroomCommand, Unit>
 {
     public async Task<Unit> Handle(RestoreClassroomCommand request,
         CancellationToken cancellationToken)
     {
-        var classroom = await context.Classrooms
-            .FirstOrDefaultAsync(e => e.ClassroomId == request.Id, cancellationToken);
-
-        if (classroom is null)
-            throw new NotFoundException(nameof(Classroom), request.Id);
-
-        classroom.IsDeleted = false;
-        context.Classrooms.Update(classroom);
-        await context.SaveChangesAsync(cancellationToken);
+        await classroomRepository.RestoreAsync(request.Id, cancellationToken);
         return Unit.Value;
     }
 }
--- Schedule/Schedule.Application/Features/Classrooms/Commands/Update/UpdateClassroomCommandHandler.cs
+++ Schedule/Schedule.Application/Features/Classrooms/Commands/Update/UpdateClassroomCommandHandler.cs
@@ -4,25 +4,19 @@ using Microsoft.EntityFrameworkCore;
 using Schedule.Core.Common.Exceptions;
 using Schedule.Core.Common.Interfaces;
 using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
 
 namespace Schedule.Application.Features.Classrooms.Commands.Update;
 
-public sealed class UpdateClassroomCommandHandler(IScheduleDbContext context, IMapper mapper)
+public sealed class UpdateClassroomCommandHandler(
+    IMapper mapper,
+    IClassroomRepository classroomRepository)
     : IRequestHandler<UpdateClassroomCommand, Unit>
 {
     public async Task<Unit> Handle(UpdateClassroomCommand request, CancellationToken cancellationToken)
     {
-        var classroom = await context.Classrooms
-            .AsNoTracking()
-            .FirstOrDefaultAsync(e => e.ClassroomId == request.Id, cancellationToken);
-
-        if (classroom is null)
-            throw new NotFoundException(nameof(Classroom), request.Id);
-
-        classroom.Cabinet = request.Cabinet;
-
-        context.Classrooms.Update(classroom);
-        await context.SaveChangesAsync(cancellationToken);
+        var classroom = mapper.Map<Classroom>(request);
+        await classroomRepository.UpdateAsync(classroom, cancellationToken);
         return Unit.Value;
     }
 }

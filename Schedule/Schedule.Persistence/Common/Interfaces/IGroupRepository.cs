5050ae8e173c92251d2a21789eab5fb2b05df576
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-20 19:41:34 +0300

    Add GroupRepository.cs

---
+++ Schedule/Schedule.Persistence/Common/Interfaces/IGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using Schedule.Core.Models;
+
+namespace Schedule.Persistence.Common.Interfaces;
+
+public interface IGroupRepository
+{
+    public Task<int> CreateAsync(Group group, CancellationToken cancellationToken = default);
+    public Task UpdateAsync(Group group, CancellationToken cancellationToken = default);
+    public Task DeleteAsync(int id, CancellationToken cancellationToken = default);
+    public Task RestoreAsync(int id, CancellationToken cancellationToken = default);
+}

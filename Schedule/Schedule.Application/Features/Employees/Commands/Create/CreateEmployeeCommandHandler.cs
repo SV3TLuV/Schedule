f7dbb46d00440bfba9d9a486c11d98eca176ac23
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-21 19:11:23 +0300

    Add commands for employee

---
+++ Schedule/Schedule.Application/Features/Employees/Commands/Create/CreateEmployeeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
+
+namespace Schedule.Application.Features.Employees.Commands.Create;
+
+public sealed class CreateEmployeeCommandHandler(
+    IEmployeeRepository employeeRepository,
+    IMapper mapper) : IRequestHandler<CreateEmployeeCommand, int>
+{
+    public async Task<int> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    {
+        var employee = mapper.Map<Employee>(request);
+        return await employeeRepository.CreateAsync(employee, cancellationToken);
+    }
+}
---
+++ Schedule/Schedule.Application/Features/Employees/Commands/Restore/RestoreEmployeeCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Employees.Commands.Restore;
+
+public sealed class RestoreEmployeeCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}

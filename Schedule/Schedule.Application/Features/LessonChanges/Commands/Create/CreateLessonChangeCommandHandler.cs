4c806c86c95739989e0b6dd6bf30c046c10b597d
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-21 12:28:59 +0300

    Update lessonChange commands

--- Schedule/Schedule.Application/Features/LessonChanges/Commands/Create/CreateLessonChangeCommandHandler.cs
+++ Schedule/Schedule.Application/Features/LessonChanges/Commands/Create/CreateLessonChangeCommandHandler.cs
@@ -2,27 +2,17 @@
 using MediatR;
 using Schedule.Core.Common.Interfaces;
 using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
 
 namespace Schedule.Application.Features.LessonChanges.Commands.Create;
 
 public sealed class CreateLessonChangeCommandHandler(
-    IScheduleDbContext context,
+    ILessonChangeRepository lessonChangeRepository,
     IMapper mapper) : IRequestHandler<CreateLessonChangeCommand, int>
 {
     public async Task<int> Handle(CreateLessonChangeCommand request, CancellationToken cancellationToken)
     {
-        await using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
-
-        try
-        {
-            var lessonChange = mapper.Map<LessonChange>(request);
-
-            await transaction.CommitAsync(cancellationToken);
-        }
-        catch
-        {
-            await transaction.RollbackAsync(cancellationToken);
-            throw;
-        }
+        var lessonChange = mapper.Map<LessonChange>(request);
+        return await lessonChangeRepository.CreateAsync(lessonChange, cancellationToken);
     }
 }
--- Schedule/Schedule.Application/Features/LessonChanges/Commands/Delete/DeleteLessonChangeCommandHandler.cs
+++ Schedule/Schedule.Application/Features/LessonChanges/Commands/Delete/DeleteLessonChangeCommandHandler.cs
@@ -1,15 +1,15 @@
 ﻿using MediatR;
 using Schedule.Core.Common.Interfaces;
+using Schedule.Persistence.Common.Interfaces;
 
 namespace Schedule.Application.Features.LessonChanges.Commands.Delete;
 
-public sealed class DeleteLessonChangeCommandHandler(
-    IScheduleDbContext context) : IRequestHandler<DeleteLessonChangeCommand, Unit>
+public sealed class DeleteLessonChangeCommandHandler(ILessonChangeRepository lessonChangeRepository)
+    : IRequestHandler<DeleteLessonChangeCommand, Unit>
 {
     public async Task<Unit> Handle(DeleteLessonChangeCommand request, CancellationToken cancellationToken)
     {
-        
-
+        await lessonChangeRepository.DeleteAsync(request.LessonChangeId, cancellationToken);
         return Unit.Value;
     }
 }

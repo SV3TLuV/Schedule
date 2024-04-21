8e08686fa477dd9f483b52ccfca5b7bbf748db39
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-21 22:42:34 +0300

    Try fix System.Reflection.AmbiguousMatchException

--- Schedule/Schedule.Application/ViewModels/TeacherClassroomIdPairViewModel.cs
+++ Schedule/Schedule.Application/ViewModels/TeacherClassroomIdPairViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Schedule.Core.Common.Interfaces;
+﻿using AutoMapper;
+using Schedule.Core.Common.Interfaces;
 using Schedule.Core.Models;
 
 namespace Schedule.Application.ViewModels;
@@ -7,4 +8,21 @@ public sealed class TeacherClassroomIdPairViewModel : IMapWith<LessonTeacherClas
 {
     public int TeacherId { get; set; }
     public int ClassroomId { get; set; }
+
+    public void Map(Profile profile)
+    {
+        profile.CreateMap<TeacherClassroomViewModel, LessonTeacherClassroom>()
+            .ForMember(lessonTeacherClassroom => lessonTeacherClassroom.TeacherId, expression =>
+                expression.MapFrom(viewModel => viewModel.Teacher.Id))
+            .ForMember(lessonTeacherClassroom => lessonTeacherClassroom.ClassroomId, expression =>
+                expression.MapFrom(viewModel => viewModel.Classroom.Id))
+            .ReverseMap();
+
+        profile.CreateMap<TeacherClassroomViewModel, LessonChangeTeacherClassroom>()
+            .ForMember(lessonTeacherClassroom => lessonTeacherClassroom.TeacherId, expression =>
+                expression.MapFrom(viewModel => viewModel.Teacher.Id))
+            .ForMember(lessonTeacherClassroom => lessonTeacherClassroom.ClassroomId, expression =>
+                expression.MapFrom(viewModel => viewModel.Classroom.Id))
+            .ReverseMap();
+    }
 }

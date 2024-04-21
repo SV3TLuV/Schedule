5deb5b78ee8407a1bbe23a89d64050d2906c31a1
SV3TLUV <Volk222kloV@gmail.com>
2024-04-21 14:54:03 +0300

    Extend AccountRepository.cs

--- Schedule/Schedule.Persistence/Common/Interfaces/IAccountRepository.cs
+++ Schedule/Schedule.Persistence/Common/Interfaces/IAccountRepository.cs
@@ -4,6 +4,13 @@ namespace Schedule.Persistence.Common.Interfaces;
 
 public interface IAccountRepository : IRepository
 {
+    public Task<int> CreateAsync(Account account, CancellationToken cancellationToken = default);
+    public Task UpdateAsync(Account account, CancellationToken cancellationToken = default);
+    public Task DeleteAsync(int accountId, CancellationToken cancellationToken = default);
+    public Task RestoreAsync(int accountId, CancellationToken cancellationToken = default);
+
     public Task<Account?> FindByEmailAsync(string email, CancellationToken cancellationToken = default);
     public Task<Account?> FindByLoginAsync(string login, CancellationToken cancellationToken = default);
+
+    public Task ChangePasswordAsync(int accountId, string password, CancellationToken cancellationToken = default);
 }

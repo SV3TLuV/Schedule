e0126b1e2820e0f6f032a4fb72b1b92dbf165410
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-21 11:52:19 +0300

    Fix IGroupTransferRepository.cs

--- Schedule/Schedule.Persistence/Common/Interfaces/IGroupTransferRepository.cs
+++ Schedule/Schedule.Persistence/Common/Interfaces/IGroupTransferRepository.cs
@@ -2,7 +2,7 @@
 
 namespace Schedule.Persistence.Common.Interfaces;
 
-public interface IGroupTransferRepository
+public interface IGroupTransferRepository : IRepository
 {
     public Task CreateAsync(GroupTransfer groupTransfer, CancellationToken cancellationToken = default);
     public Task MarkAsTransferedAsync(GroupTransfer groupTransfer, CancellationToken cancellationToken = default);

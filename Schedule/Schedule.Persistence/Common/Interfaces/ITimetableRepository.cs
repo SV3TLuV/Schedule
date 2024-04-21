91162e0cd1f8373f3265ad28d68879529c3a66a0
SV3TLUV <Volk222kloV@gmail.com>
2024-04-21 19:39:40 +0300

    Extend TimetableRepository.cs

--- Schedule/Schedule.Persistence/Common/Interfaces/ITimetableRepository.cs
+++ Schedule/Schedule.Persistence/Common/Interfaces/ITimetableRepository.cs
@@ -6,4 +6,6 @@ public interface ITimetableRepository : IRepository
 {
     public Task<int> CreateAsync(Timetable timetable, CancellationToken cancellationToken = default);
     public Task UpdateAsync(Timetable timetable, CancellationToken cancellationToken = default);
+
+    public Task CreateForGroupAsync(int groupId, CancellationToken cancellationToken = default);
 }

4d76752d1b672bd45d4085d32a56cff783a1f756
SV3TLUV <Volk222kloV@gmail.com>
2024-04-21 18:50:33 +0300

    Add teacher commands

---
+++ Schedule/Schedule.Application/Features/Teachers/Commands/Create/CreateTeacherCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MediatR;
+using Schedule.Core.Models;
+using Schedule.Persistence.Common.Interfaces;
+
+namespace Schedule.Application.Features.Teachers.Commands.Create;
+
+public sealed class CreateTeacherCommandHandler(
+    ITeacherRepository teacherRepository,
+    IMapper mapper) : IRequestHandler<CreateTeacherCommand, int>
+{
+    public async Task<int> Handle(CreateTeacherCommand request, CancellationToken cancellationToken)
+    {
+        var teacher = mapper.Map<Teacher>(request);
+        return await teacherRepository.CreateAsync(teacher, cancellationToken);
+    }
+}
---
+++ Schedule/Schedule.Application/Features/Teachers/Commands/Delete/DeleteTeacherCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Teachers.Commands.Delete;
+
+public sealed class DeleteTeacherCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}

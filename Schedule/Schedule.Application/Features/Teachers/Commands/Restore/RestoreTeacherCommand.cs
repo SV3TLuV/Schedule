522ef38685b3ed276d27cec595ce90ef2916791c
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-04-21 18:50:33 +0300

    Add teacher commands

---
+++ Schedule/Schedule.Application/Features/Teachers/Commands/Restore/RestoreTeacherCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Teachers.Commands.Restore;
+
+public sealed class RestoreTeacherCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
---
+++ Schedule/Schedule.Application/Features/Teachers/Commands/Update/UpdateTeacherCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Schedule.Core.Common.Interfaces;
+using Schedule.Core.Models;
+
+namespace Schedule.Application.Features.Teachers.Commands.Update;
+
+public sealed class UpdateTeacherCommand : IRequest<Unit>, IMapWith<Teacher>
+{
+
+}

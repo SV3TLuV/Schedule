a2a1701683c8b02822765663ddbd8160c9e60f88
noH4uK90 <noh4uk90@gmail.com>
2024-04-21 14:06:22 +0300

    Update Lesson

--- Schedule/Schedule.Application/ViewModels/LessonViewModel.cs
+++ Schedule/Schedule.Application/ViewModels/LessonViewModel.cs
@@ -16,15 +16,11 @@ public class LessonViewModel : IMapWith<Lesson>
 
     public int TimetableId { get; set; }
     
-    public List<int> TeacherIds { get; set; } = null!;
-    
-    public List<int> ClassroomIds { get; set; } = null!;
-    
     public LessonChangeViewModel? LessonChange { get; set; }
 
-    public TimeOnly? TimeStart { get; set; }
+    public TimeOnly TimeStart { get; set; }
     
-    public TimeOnly? TimeEnd { get; set; }
+    public TimeOnly TimeEnd { get; set; }
 
     public void Map(Profile profile)
     {

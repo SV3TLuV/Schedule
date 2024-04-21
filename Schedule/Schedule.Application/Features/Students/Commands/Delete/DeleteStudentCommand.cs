f2f43fdba79e97255c95e3ef153470de144a4975
SV3TLUV <Volk222kloV@gmail.com>
2024-04-21 19:05:58 +0300

    Add student commands

---
+++ Schedule/Schedule.Application/Features/Students/Commands/Delete/DeleteStudentCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Schedule.Application.Features.Students.Commands.Delete;
+
+public sealed class DeleteStudentCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}

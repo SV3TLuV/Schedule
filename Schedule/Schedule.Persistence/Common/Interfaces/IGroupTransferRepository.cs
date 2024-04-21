516d32f9ce037255c9058406e6c8c0b26409a913
SV3TLUV <Volk222kloV@gmail.com>
2024-04-21 11:46:27 +0300

    Add IGroupTransferRepository.cs

---
+++ Schedule/Schedule.Persistence/Common/Interfaces/IGroupTransferRepository.cs
@@ -0,0 +1,10 @@
+﻿using Schedule.Core.Models;
+
+namespace Schedule.Persistence.Common.Interfaces;
+
+public interface IGroupTransferRepository
+{
+    public Task CreateAsync(GroupTransfer groupTransfer, CancellationToken cancellationToken = default);
+    public Task MarkAsTransferedAsync(GroupTransfer groupTransfer, CancellationToken cancellationToken = default);
+    public Task DeleteAsync(GroupTransfer groupTransfer, CancellationToken cancellationToken = default);
+}

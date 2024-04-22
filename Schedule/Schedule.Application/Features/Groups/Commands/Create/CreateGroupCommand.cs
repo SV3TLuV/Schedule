992b820ff3b3228425482d9036cc853744560706
SV3TLUV <Volk222kloV@gmail.com>
2024-04-22 19:05:52 +0300

    Fix CreateGroupCommand.cs, UpdateGroupCommand.cs

--- Schedule/Schedule.Application/Features/Groups/Commands/Create/CreateGroupCommand.cs
+++ Schedule/Schedule.Application/Features/Groups/Commands/Create/CreateGroupCommand.cs
@@ -10,5 +10,4 @@ public sealed class CreateGroupCommand : IRequest<int>, IMapWith<Group>
     public required int EnrollmentYear { get; set; }
     public required int SpecialityId { get; set; }
     public bool IsAfterEleven { get; set; }
-    public required int TermId { get; set; }
 }

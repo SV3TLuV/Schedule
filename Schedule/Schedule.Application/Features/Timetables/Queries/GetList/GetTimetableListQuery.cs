5a3127f6f92e6e3036b326040f1ef837ce19a3e0
SV3TLUV <67441287+SV3TLuV@users.noreply.github.com>
2024-05-03 21:33:08 +0300

    add GetTimetableListQuery.cs

---
+++ Schedule/Schedule.Application/Features/Timetables/Queries/GetList/GetTimetableListQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Schedule.Application.ViewModels;
+using Schedule.Core.Common.Enums;
+
+namespace Schedule.Application.Features.Timetables.Queries.GetList;
+
+public class GetTimetableListQuery : IRequest<ICollection<TimetableViewModel>>
+{
+    public int? GroupId { get; set; }
+    public int? DayId { get; set; }
+    public WeekType? WeekTypeId { get; set; }
+    public DateOnly? Date { get; set; }
+}
